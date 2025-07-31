b1b7eb9ddf06093278f3295e821b92ba977d58a1
Alionit01 <fotopob33@gmail.com>
2025-08-01 04:19:55 +0500

    implemented random function

--- practiseApps/firstApp/flutter_application_1/lib/screens/Dashboard.dart
+++ practiseApps/firstApp/flutter_application_1/lib/screens/Dashboard.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class Dashboard extends StatelessWidget {
@@ -11,15 +13,16 @@ class Dashboard extends StatelessWidget {
             backgroundColor: Colors.blueGrey,
          ),
          body: Center(
-            child: Text.rich(
-               TextSpan(text: "The",
-               children: [
-                  TextSpan(text: "Ali", style: TextStyle(fontSize: 50.0, fontWeight: FontWeight.bold)),
-                  TextSpan(text: "App", style: TextStyle(fontSize: 30.0, color: Colors.amber[400])),
-               ]
-               )
-            ),
+            child: Text("Random value: ${getNumber()}")
          ),
         );
   }
 }
+//easy way
+// int getNumber(){
+//   Random random = new Random();
+//   var number = random.nextInt(10);
+//   return number;
+// }
+//better way
+int getNumber(){return Random().nextInt(10);}
