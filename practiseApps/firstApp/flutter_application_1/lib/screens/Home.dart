b08b3b021500de23e606771f8a4981aafec4cc4f
Alionit01 <fotopob33@gmail.com>
2025-08-02 12:15:04 +0500

    Implemented rows and columns

--- practiseApps/firstApp/flutter_application_1/lib/screens/Home.dart
+++ practiseApps/firstApp/flutter_application_1/lib/screens/Home.dart
@@ -6,21 +6,29 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Home"),
-        leading: IconButton(onPressed: (){}, icon: Icon(Icons.menu)),
-        actions: [
-          IconButton(onPressed: (){}, icon: Icon(Icons.card_travel)),
-          IconButton(onPressed: (){}, icon: Icon(Icons.search)),
-          IconButton(onPressed: (){}, icon: Icon(Icons.menu_book)),
-        ],
-      ),
-      body: Center(
-       child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+      appBar: AppBar(title: Text("Home"), ),
+      body: Container(
+       child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
-          Text("App Bar", style: TextStyle(fontSize: 22.0),),
-          Text("Ali yoyo", style: TextStyle(fontWeight: FontWeight.bold),),
+          Column(
+            children: [
+              Icon(Icons.phone, size: 35.00,),
+              Text("Phone"),
+            ],
+          ),
+          Column(
+            children: [
+              Icon(Icons.card_travel, size: 35.00,),
+              Text("Cart"),
+            ],
+          ),
+          Column(
+            children: [
+              Icon(Icons.share, size: 35.00,),
+              Text("Share"),
+            ],
+          ),
         ],
        ),
       ),
