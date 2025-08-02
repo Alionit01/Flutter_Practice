5987adca672a265880e631e3ab08927939fb9964
Alionit01 <fotopob33@gmail.com>
2025-08-02 21:11:19 +0500

    implemened stack

--- practiseApps/firstApp/flutter_application_1/lib/screens/Home.dart
+++ practiseApps/firstApp/flutter_application_1/lib/screens/Home.dart
@@ -3,53 +3,40 @@ import 'package:flutter/material.dart';
 class Home extends StatelessWidget {
   Home({super.key});
 
-  List<String> products = ["Bed", "Table", "Chair"];
-  List<String> productDetails = ["King Size Bed", "Wooden Table", "Wooden Chair"];
-  List<int> price = [30000, 5000, 8000];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Navigation Drawer"),),
-      drawer: Drawer(
-        child: ListView(
+      appBar: AppBar(
+        title: Text("Stack"),
+        centerTitle: true,
+        backgroundColor: Colors.black38,
+      ),
+      backgroundColor: Colors.grey.shade800,
+      body: Center(
+        child: Stack(
+          alignment: Alignment.center,
           children: [
-            UserAccountsDrawerHeader(
-              accountEmail: Text("iamAlive@gmail.com"),
-              accountName: Text("TUPAC"),
-              currentAccountPicture: CircleAvatar(foregroundImage: AssetImage("assets/smileyFace.png"),),
-              
+            Container(
+              width: 300.0,
+              height: 200,
+              color: Colors.green,
             ),
-            ListTile(leading: Icon(Icons.home), title: Text("Home"),),
-            ListTile(leading: Icon(Icons.shopping_cart), title: Text("Shop"),),
-            ListTile(leading: Icon(Icons.favorite), title: Text("Favorites"),),
-            Padding(
-            padding: const EdgeInsets.all(14.0),
-            child: Text("Labels"),
+             Positioned(
+              bottom: 0.0,
+               child: Container(
+                width: 150.0,
+                height: 100,
+                color: Colors.yellow,
+                           ),
+             ),
+            Container(
+              width: 100.0,
+              height: 75,
+              color: Colors.redAccent,
             ),
-            ListTile(leading: Icon(Icons.label), title: Text("Home"),),
-            ListTile(leading: Icon(Icons.label), title: Text("Shop"),),
-            ListTile(leading: Icon(Icons.label), title: Text("Favorites"),),
-             
           ],
         ),
       ),
-
-     body: Container(
-      child: ListView.builder(
-        itemCount: products.length,
-        itemBuilder: (context, index){
-          return ListTile(
-            leading: CircleAvatar(child: Text(products[index][0]),),
-            title: Text(products[index]),
-            subtitle: Text(productDetails[index]),
-            trailing: Text(price[index].toString()),
-            hoverColor: Colors.blueGrey,
-          );
-        }
-
-        ),
-     )
     );
   }
 }
