058f41e2e69e607a8ee3098046a80dc93a5ca6cf
Alionit01 <fotopob33@gmail.com>
2025-08-02 13:14:28 +0500

    Implemented dynamic list view

--- practiseApps/firstApp/flutter_application_1/lib/screens/Home.dart
+++ practiseApps/firstApp/flutter_application_1/lib/screens/Home.dart
@@ -1,37 +1,30 @@
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
-  const Home({super.key});
+  Home({super.key});
+
+  List<String> products = ["Bed", "Table", "Chair"];
+  List<String> productDetails = ["King Size Bed", "Wooden Table", "Wooden Chair"];
+  List<int> price = [30000, 5000, 8000];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
      body: Container(
-      child: ListView(
-        children: [
-          ListTile(
-            leading: CircleAvatar(child: Icon(Icons.alarm_on_sharp), backgroundColor: Colors.green[200],),
-            title: Text("Sales"),
-            subtitle: Text("Sales of week"),
-            trailing: Text("3500"),
-            onTap: (){},
-          ),
-           ListTile(
-            leading: Icon(Icons.alarm_on_sharp),
-            title: Text("Sales"),
-            subtitle: Text("Customers"),
-            trailing: Text("30"),
-            onTap: (){},
-          ), ListTile(
-            leading: Icon(Icons.alarm_on_sharp),
-            title: Text("Sales"),
-            subtitle: Text("Profit"),
-            trailing: Text("500"),
-            onTap: (){},
-          ),
-        ],
-      ),
-     ),
+      child: ListView.builder(
+        itemCount: products.length,
+        itemBuilder: (context, index){
+          return ListTile(
+            leading: CircleAvatar(child: Text(products[index][0]),),
+            title: Text(products[index]),
+            subtitle: Text(productDetails[index]),
+            trailing: Text(price[index].toString()),
+            hoverColor: Colors.blueGrey,
+          );
+        }
+
+        ),
+     )
     );
   }
 }
