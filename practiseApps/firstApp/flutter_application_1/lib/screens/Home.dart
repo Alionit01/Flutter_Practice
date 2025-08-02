06deaf68ce9ee2a52b9a4e00eb5ca6656b31ad17
Alionit01 <fotopob33@gmail.com>
2025-08-02 19:56:07 +0500

    Implemented Navigation Drawer

--- practiseApps/firstApp/flutter_application_1/lib/screens/Home.dart
+++ practiseApps/firstApp/flutter_application_1/lib/screens/Home.dart
@@ -10,6 +10,31 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(title: Text("Navigation Drawer"),),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            UserAccountsDrawerHeader(
+              accountEmail: Text("iamAlive@gmail.com"),
+              accountName: Text("TUPAC"),
+              currentAccountPicture: CircleAvatar(foregroundImage: AssetImage("assets/smileyFace.png"),),
+              
+            ),
+            ListTile(leading: Icon(Icons.home), title: Text("Home"),),
+            ListTile(leading: Icon(Icons.shopping_cart), title: Text("Shop"),),
+            ListTile(leading: Icon(Icons.favorite), title: Text("Favorites"),),
+            Padding(
+            padding: const EdgeInsets.all(14.0),
+            child: Text("Labels"),
+            ),
+            ListTile(leading: Icon(Icons.label), title: Text("Home"),),
+            ListTile(leading: Icon(Icons.label), title: Text("Shop"),),
+            ListTile(leading: Icon(Icons.label), title: Text("Favorites"),),
+             
+          ],
+        ),
+      ),
+
      body: Container(
       child: ListView.builder(
         itemCount: products.length,
