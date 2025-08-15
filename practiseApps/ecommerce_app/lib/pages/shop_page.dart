75323274781886ad2e24a5f7564e4efc01d1cbea
Alionit01 <fotopob33@gmail.com>
2025-08-15 21:18:46 +0500

    Implemented searchBar

--- practiseApps/ecommerce_app/lib/pages/shop_page.dart
+++ practiseApps/ecommerce_app/lib/pages/shop_page.dart
@@ -10,6 +10,57 @@ class ShopPage extends StatefulWidget {
 class _ShopPageState extends State<ShopPage> {
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text("Shop"),);
+    return Column(
+      children: [
+        Container(
+          padding: EdgeInsets.all(12),
+          margin: const EdgeInsets.symmetric(horizontal: 25),
+          decoration: BoxDecoration(
+            color: Colors.grey[100],
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text("Search"),
+              Icon(
+                Icons.search,
+                color: Colors.grey,
+                ),
+            ],
+          ),
+        ),
+
+      Padding(
+        padding: EdgeInsetsGeometry.symmetric(vertical: 25.0),
+        child: Text(
+          "It is the stop that kills you.. not the fall",
+          style: TextStyle(color: Colors.grey[600]),
+          ),
+         ),
+
+      Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 25.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.end,
+          children: const [
+          Text("Hot Picks",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 24,
+            ),
+          ),
+           Text("See all",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Colors.blue,
+            ),
+          ),
+          ],
+        ),
+      )
+
+      ],
+    );
   }
 }
